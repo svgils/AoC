2b5570f79214fb5138da00542770bd98b73309d0
Stijn van Gils <stijn-28@hotmail.com>
2023-12-04 15:13:33 +0100

    day04 cleanup.

--- 2023/day04/src/main.rs
+++ 2023/day04/src/main.rs
@@ -3,7 +3,7 @@ use std::fs;
 
 const INPUT: &str = "./input";
 
-fn stack_cards(card: usize, orig_cards: &Vec<i32>) -> i32 {
+fn stack_cards(card: usize, orig_cards: &Vec<u32>) -> u32 {
     let mut sum = 1;
 
     for i in 1..orig_cards[card] + 1 {
@@ -13,22 +13,26 @@ fn stack_cards(card: usize, orig_cards: &Vec<i32>) -> i32 {
     return sum;
 }
 
-fn part2(input: &str) -> i32 {
+fn main() {
+    let input = fs::read_to_string(INPUT).unwrap();
+    let mut part1_res = 0;
+    let mut part2_res = 0;
     let re = Regex::new(r"\d+").unwrap();
-    let orig_cards: Vec<i32> = input
+
+    let orig_cards: Vec<u32> = input
         .split("\n")
-        .filter_map(|line| -> Option<i32> {
+        .filter_map(|line| -> Option<u32> {
             if line.is_empty() {
                 return None;
             };
             let mut numbers = line.split(":").last().unwrap().split("|");
-            let winning: Vec<i32> = re
+            let winning: Vec<u32> = re
                 .find_iter(numbers.next().unwrap())
-                .map(|x| x.as_str().parse::<i32>().unwrap())
+                .map(|x| x.as_str().parse::<u32>().unwrap())
                 .collect();
             let mut hits = 0;
             re.find_iter(numbers.last().unwrap()).for_each(|m| {
-                let n = m.as_str().parse::<i32>().unwrap();
+                let n = m.as_str().parse::<u32>().unwrap();
                 if winning.iter().any(|x| *x == n) {
                     hits += 1;
                 }
@@ -37,43 +41,13 @@ fn part2(input: &str) -> i32 {
         })
         .collect();
 
-    let mut res = 0;
     for i in 0..orig_cards.len() {
-        res += stack_cards(i, &orig_cards);
+        part2_res += stack_cards(i, &orig_cards);
+        if orig_cards[i] > 0 {
+            part1_res += u32::pow(2, orig_cards[i] - 1);
+        }
     }
 
-    return res;
-}
-
-fn main() {
-    let input = fs::read_to_string(INPUT).unwrap();
-
-    let lines = input.split("\n");
-
-    let mut res = 0;
-
-    lines.for_each(|line| {
-        if !line.is_empty() {
-            let mut numbers = line.split(":").last().unwrap().split("|");
-            let re = Regex::new(r"\d+").unwrap();
-            let winning: Vec<i32> = re
-                .find_iter(numbers.next().unwrap())
-                .map(|x| x.as_str().parse::<i32>().unwrap())
-                .collect();
-            let mut hits = 0;
-            re.find_iter(numbers.last().unwrap()).for_each(|m| {
-                let n = m.as_str().parse::<i32>().unwrap();
-                if winning.iter().any(|x| *x == n) {
-                    hits += 1;
-                }
-            });
-            let mut running = if hits > 0 { 1 } else { 0 };
-            for _i in 1..hits {
-                running *= 2;
-            }
-            res += running;
-        }
-    });
-    println!("Part1: {}", res);
-    println!("Part2: {}", part2(&input));
+    println!("Part1: {}", part1_res);
+    println!("Part2: {}", part2_res);
 }

f0a1809a4461b5daae7c4db1d82e16c3ce4d7e5c
Stijn <stijn-28@hotmail.com>
2023-12-11 02:09:12 +0100

    Work on 10.2

--- 2023/day10/src/main.rs
+++ 2023/day10/src/main.rs
@@ -1,13 +1,30 @@
-use std::{collections::HashMap, fs};
+#[macro_use]
+extern crate lazy_static;
+use std::{
+    collections::{HashMap, HashSet},
+    fs,
+};
 
 const INPUT: &str = "./input";
 
-#[derive(Hash, Eq, PartialEq)]
+#[derive(Debug, Hash, Eq, PartialEq, Clone, Copy)]
 enum Direction {
     North,
     East,
     South,
     West,
+}
+
+impl Direction {
+    fn from_u8(value: u8) -> Direction {
+        match value {
+            0 => Direction::North,
+            1 => Direction::East,
+            2 => Direction::South,
+            3 => Direction::West,
+            _ => Direction::North,
+        }
+    }
 }
 
 fn find_start(map: &Vec<Vec<char>>) -> (usize, usize) {
@@ -21,8 +38,30 @@ fn find_start(map: &Vec<Vec<char>>) -> (usize, usize) {
     return (0, 0);
 }
 
+fn next_direction(
+    direction: Direction,
+    pipe: &char,
+    map: &mut HashMap<char, (Direction, Direction)>,
+) -> Direction {
+    lazy_static! {
+        static ref INVERSE_MAP: HashMap<Direction, Direction> = HashMap::from([
+            (Direction::North, Direction::South),
+            (Direction::East, Direction::West),
+            (Direction::South, Direction::North),
+            (Direction::West, Direction::East),
+        ]);
+    }
+
+    return if INVERSE_MAP[&direction] != map[pipe].0 {
+        map[pipe].0
+    } else {
+        map[pipe].1
+    };
+}
+
 fn main() {
-    let direction_map = HashMap::from([
+    // Map pipe sections input and output heading and rotational value.
+    let mut direction_map = HashMap::from([
         ('-', (Direction::East, Direction::West)),
         ('|', (Direction::North, Direction::South)),
         ('L', (Direction::North, Direction::East)),
@@ -30,21 +69,6 @@ fn main() {
         ('7', (Direction::West, Direction::South)),
         ('F', (Direction::East, Direction::South)),
     ]);
-
-    let inverse_map = HashMap::from([
-        (Direction::North, Direction::South),
-        (Direction::East, Direction::West),
-        (Direction::South, Direction::North),
-        (Direction::West, Direction::East),
-    ]);
-
-    let next_direction = |direction: &Direction, pipe: &char| -> &Direction {
-        return if inverse_map[direction] != direction_map[pipe].0 {
-            &direction_map[pipe].0
-        } else {
-            &direction_map[pipe].1
-        };
-    };
 
     let offset_map: HashMap<Direction, (i64, i64)> = HashMap::from([
         (Direction::North, (0, -1)),
@@ -106,22 +130,113 @@ fn main() {
         starts.push(&Direction::West);
     }
 
-    let mut walkers = vec![(start, starts[0]), (start, starts[1])];
+    direction_map.entry('S').or_insert((*starts[0], *starts[1]));
+
+    struct Walker {
+        pos: (usize, usize),
+        dir: Direction,
+        rotation: i16,
+        inside_tiles: HashSet<(usize, usize)>,
+    }
+
+    println!("{:?}", starts);
+
+    let mut walkers = vec![
+        Walker {
+            pos: start,
+            dir: *starts[0],
+            rotation: 0,
+            inside_tiles: HashSet::new(),
+        },
+        Walker {
+            pos: start,
+            dir: *starts[1],
+            rotation: 0,
+            inside_tiles: HashSet::new(),
+        },
+    ];
     // Find 2 possible start directions
 
+    let mut visited_tiles: HashSet<(usize, usize)> = HashSet::new();
     let mut steps = 0;
     loop {
         for walker in &mut walkers {
-            let next = next_point(&walker.1, walker.0);
-            walker.0 = next;
-            walker.1 = next_direction(&walker.1, &index_map(next));
+            let old_dir = walker.dir;
+            let next = next_point(&walker.dir, walker.pos);
+            walker.pos = next;
+            walker.dir = next_direction(walker.dir, &index_map(next), &mut direction_map);
+            walker.rotation += (walker.dir as i16 * 90 - old_dir as i16 * 90) % 180;
+            // Add tile to right to inside tiles list
+            // println!("{:?}{}", old_dir, old_dir as u8);
+            let right1 = next_point(&Direction::from_u8((walker.dir as u8 + 1) % 4), walker.pos);
+            let right2 = next_point(&Direction::from_u8((old_dir as u8 + 1) % 4), walker.pos);
+            // println!("Right: {:?}", right1);
+            if right1.0 < (input[0].len() - 1) && right1.1 < (input.len() - 1) {
+                println!("Adding {:?} to inside", right1);
+                walker.inside_tiles.insert(right1);
+            }
+            if right2.0 < (input[0].len() - 1) && right2.1 < (input.len() - 1) {
+                println!("Adding {:?} to inside", right2);
+                walker.inside_tiles.insert(right2);
+            }
+            println!("Adding {:?} to visited", walker.pos);
+            visited_tiles.insert(walker.pos);
         }
         steps += 1;
-
-        if walkers[0].0 == walkers[1].0 {
+        if walkers[0].pos == start {
             break;
+        }
+
+        if walkers[0].pos == walkers[1].pos {
+            println!("Part 1: {}", steps);
         }
     }
 
-    println!("Part 1: {}", steps);
+    let clockwise_walker = if walkers[0].rotation > 0 {
+        &walkers[0]
+    } else {
+        &walkers[1]
+    };
+
+    let mut fill_map: HashMap<(usize, usize), bool> = HashMap::new();
+    let mut open = false;
+    let mut was_prev_open = false;
+    for y in 0..input.len() {
+        for x in 0..input[0].len() {
+            if visited_tiles.contains(&(x, y)) {
+                *fill_map.entry((x, y)).or_insert(false) = false;
+            } else {
+                *fill_map.entry((x, y)).or_insert(open) = open;
+            }
+        }
+    }
+    let mut open = false;
+    let mut was_prev_open = false;
+    for x in 0..input[0].len() {
+        for y in 0..input.len() {
+            if visited_tiles.contains(&(x, y)) {
+                if !was_prev_open {
+                    open = !open;
+                    was_prev_open = open;
+                }
+                // *fill_map.entry((x, y)).or_insert(false) = false;
+            } else {
+                was_prev_open = false;
+                *fill_map.entry((x, y)).or_insert(open) &= open;
+            }
+        }
+    }
+
+    // let unique: HashSet<&(usize, usize)> = clockwise_walker
+    //     .inside_tiles
+    //     .difference(&visited_tiles)
+    //     .collect();
+    let mut sum = 0;
+    for val in fill_map.iter() {
+        if *val.1 {
+            sum += 1;
+        }
+    }
+
+    println!("Part 2: {}", sum);
 }

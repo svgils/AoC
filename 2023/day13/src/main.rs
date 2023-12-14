677e77f1e6744fc8ef88dd5690a9dc186b94e7e2
Stijn van Gils <stijn-28@hotmail.com>
2023-12-14 17:33:42 +0100

    Day 13.1

---
+++ 2023/day13/src/main.rs
@@ -0,0 +1,82 @@
+use std::{cmp::min, fs};
+
+const INPUT: &str = "./input";
+
+fn main() {
+    let grids: Vec<Vec<Vec<bool>>> = fs::read_to_string(INPUT)
+        .unwrap()
+        .split("\n\n")
+        .filter_map(|section| {
+            if section.is_empty() {
+                return None;
+            }
+            return Some(
+                section
+                    .split("\n")
+                    .filter_map(|l| {
+                        if l.is_empty() {
+                            return None;
+                        }
+                        return Some(
+                            l.chars()
+                                .map(|c| if c == '#' { true } else { false })
+                                .collect(),
+                        );
+                    })
+                    .collect(),
+            );
+        })
+        .collect();
+
+    let (res1, res2) = grids.iter().fold((0, 0), |acc, grid| {
+        let mut new_res = (0, 0);
+        let mut p2_satisfied = false;
+        for x in 0..(grid[0].len() - 1) {
+            let mut equal = true;
+            let mut differrent = 0;
+            for i in 0..min(x + 1, grid[0].len() - x - 1) {
+                for y in 0..grid.len() {
+                    let eq = !(grid[y][x - i] ^ grid[y][x + i + 1]);
+                    if !eq {
+                        differrent += 1;
+                    }
+                    equal &= eq;
+                }
+            }
+            if differrent <= 1 && !p2_satisfied {
+                new_res.1 = acc.1 + x + 1;
+                p2_satisfied = true;
+            }
+            if equal {
+                new_res.0 = acc.0 + x + 1;
+                break;
+            }
+        }
+
+        for y in 0..(grid.len() - 1) {
+            let mut equal = true;
+            let mut differrent = 0;
+            for i in 0..min(y + 1, grid.len() - y - 1) {
+                for x in 0..grid[0].len() {
+                    let eq = !(grid[y - i][x] ^ grid[y + i + 1][x]);
+                    if !eq {
+                        differrent += 1;
+                    }
+                    equal &= eq;
+                }
+            }
+            if differrent <= 1 && !p2_satisfied {
+                new_res.1 = acc.1 + ((y + 1) * 100);
+                p2_satisfied = true;
+            }
+            if equal {
+                new_res.0 = acc.0 + ((y + 1) * 100);
+                break;
+            }
+        }
+        return new_res;
+    });
+
+    println!("Part 1: {}", res1);
+    println!("Part 2: {}", res2);
+}
